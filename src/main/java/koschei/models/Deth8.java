4564aeab9b5da62741ad03df2faca2fca9296496
Rostislav1986 <SorryRost@yandex.ru>
2022-09-27 17:37:03 +0300

    7-8)

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,7 +1,7 @@
 package koschei.models;
 
 import org.springframework.stereotype.Component;
-
+@Component
 public class Deth8 {
     public Deth8(){
 
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
+    @Autowired
+    private Deth8 deth;
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
